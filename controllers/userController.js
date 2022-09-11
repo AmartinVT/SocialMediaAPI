c575ce824d58bc7f24f5802802436de2c3775748
Adam Martin <adammartinmac@adams-air.lan>
2022-09-11 12:48:24 -0400

    Added friend delete to userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -55,4 +55,35 @@ module.exports = {
         .then(() => res.json({ message: 'User and associated thoughts deleted!' }))
         .catch((err) => res.status(500).json(err));
     },
+
+  //Add one friend
+  addFriend(req, res) {
+    User.findOneAndUpdate(
+      { _id: req.params.userId },
+      { $addToSet: { friends: req.params.friendId } },
+      { runValidators: true, new: true }
+    )
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: "No user found with this ID!" })
+          : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+  
+  // Delete one friend
+  deleteFriend(req, res) {
+    User.findOneAndUpdate(
+      { _id: req.params.userId },
+      { $pull: { friends: req.params.friendId } },
+      { new: true }
+    )
+      .then(
+        (user) =>
+          !user
+            ? res.status(404).json({ message: "No user found with this ID!" })
+            : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
 };
