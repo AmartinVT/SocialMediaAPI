8e65ea569e07abf2d7f303bef42e64751b03547d
Adam Martin <adammartinmac@adams-air.lan>
2022-09-11 11:31:27 -0400

    Added update 1 user by ID and delete user + thoughts based on student activity 26

---
+++ controllers/userController.js
@@ -0,0 +1,58 @@
+const { User, Thought } = require('../models/User');
+
+module.exports = {
+  // Requests all users
+  getUsers(req, res) {
+    User.find()
+      .then((users) => res.json(users))
+      .catch((err) => res.status(500).json(err));
+  },
+  
+  // Requests one user
+  getSingleUser(req, res) {
+    User.findOne({ _id: req.params.userId })
+      .select('-__v')
+      .populate('thoughts')
+      .populte('friends')
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: 'No user found with that ID' })
+          : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+
+  // Create a new user
+  createUser(req, res) {
+    User.create(req.body)
+      .then((dbUserData) => res.json(dbUserData))
+      .catch((err) => res.status(500).json(err));
+  },
+
+  // Update one user
+  updateUser(req, res) {
+    User.findOneAndUpdate(
+      { _id: req.params.userId },
+      { $set: req.body },
+      { runValidators: true, new: true }
+    )
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: "No user found with this ID!" })
+          : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
+    },
+
+  // Delete a user and associated thoughts
+  deleteUser(req, res) {
+    User.findOneAndDelete({ _id: req.params.userId })
+        .then((user) =>
+        !user
+            ? res.status(404).json({ message: 'No user with that ID' })
+            : Thoughts.deleteMany({ _id: { $in: user.thoughts } })
+        )
+        .then(() => res.json({ message: 'User and associated thoughts deleted!' }))
+        .catch((err) => res.status(500).json(err));
+    },
+};
