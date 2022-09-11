d91e56a53fa4bed0722de8e522c9e07749a1939a
Adam Martin <adammartinmac@adams-air.lan>
2022-09-11 13:21:00 -0400

    Removed erronous code from thoughtController.js

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -52,21 +52,6 @@ module.exports = {
       )
       .catch((err) => res.status(500).json(err));
    },
-
-  //Add one friend
-  addFriend(req, res) {
-    User.findOneAndUpdate(
-      { _id: req.params.userId },
-      { $addToSet: { friends: req.params.friendId } },
-      { runValidators: true, new: true }
-    )
-      .then((user) =>
-        !user
-          ? res.status(404).json({ message: "No user found with this ID!" })
-          : res.json(user)
-      )
-      .catch((err) => res.status(500).json(err));
-  },
   
   // Delete one thought
   deleteThought(req, res) {
